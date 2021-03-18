766b735d0e5cf987d057200a83186a1048c2fe14
yamada667 <tineyreal67pics+divpo@gmail.com>
2021-03-18 12:20:00 +0900

    投稿の修正、削除

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,6 +25,24 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy
+    article = Article.find(params[:id])
+    article.destroy
+    flash[:danger] = "Article was successfully deleted"
+    redirect_to root_path
+  end
+
+  def update
+    article = Article.find(params[:id])
+    if article.update(article_params)
+      flash[:success] = "Article was successfully updated"
+      redirect_to article_path(article)
+    else
+      render 'edit'
+    end
+  end
+
+
   private
   def article_params
     params.require(:article).permit(:title,:description,:image).merge(user_id: current_user.id)
