2c3d6be5373096cb3da3aef634ed54b025fb346a
yamada667 <tineyreal67pics+divpo@gmail.com>
2021-03-18 11:30:21 +0900

    投稿機能

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,25 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
+  end
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.create(article_params)
+    if @article.save
+      flash[:success] = "Article was successfully created"
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title,:description,:image)
   end
 
 end
