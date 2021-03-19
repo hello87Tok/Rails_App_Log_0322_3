ce4f0b95226f07e7f14ae566a4611d129325b018
yamada667 <tineyreal67pics+divpo@gmail.com>
2021-03-19 16:10:44 +0900

    コメント

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,2 +1,14 @@
 class Keyword < ApplicationRecord
+
+# 1.キーワードデータベース
+# 2.配列格納
+# 3.ランダム検出→検索→結果→引用差し込み
+# この機能は必要だろうか？各人、自分で検索するのでは？それよりも愛着が持てるアプリになったほうがいいのでは？
+
+# 参考：
+# Article.pluck(:name).map(&:to_i)
+
+# fruits = ["apple", "orange", "banana", "kiwi"]
+# p fruits.shuffle #-> ["banana", "kiwi", "apple", "orange"]
+
 end
