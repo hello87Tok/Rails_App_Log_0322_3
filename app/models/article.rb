397f8f66c881249468d8341e19e3e80f4762ed7c
yamada667 <tineyreal67pics+divpo@gmail.com>
2021-03-19 17:28:50 +0900

    validate

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,6 +5,9 @@ class Article < ApplicationRecord
   has_many :article_categories
   has_many :categories,through: :article_categories
 
+  validates :title, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 200}
+  validates :user_id, presence: true
 
   def self.search(search)
     return Article.all unless search
