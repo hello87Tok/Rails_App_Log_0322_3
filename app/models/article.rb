b47ee03fb41b38009bb57de84b1b956e6cda678e
yamada667 <tineyreal67pics+divpo@gmail.com>
2021-03-22 20:19:06 +0900

    記事を消したらコメントも削除

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :user
-  has_many :comments
+  has_many :comments,dependent: :destroy
 
   has_many :article_categories
   has_many :categories,through: :article_categories
