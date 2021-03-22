d030e4251bc2e7a4a264d1643dc358338a4707d5
yamada667 <tineyreal67pics+divpo@gmail.com>
2021-03-22 13:14:41 +0900

    css移動

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,7 @@ require("channels")
 require('custom')
 
 import '@fortawesome/fontawesome-free/js/all';
+import "packs/style.css";
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
